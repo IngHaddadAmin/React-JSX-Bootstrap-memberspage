12aebd3ef3f50d18295fecbe339750eec24cc478
HADDAD Amin <63170278+IngHaddadAmin@users.noreply.github.com>
2024-02-14 19:03:35 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Introd from './components/Introd'
+import Txt from './components/Txt'
+import Social from './components/Social'
+import Team from './components/Team';
+import HeaderTeam from './components/HeaderTeam';
+
+
+//import './App.css';
+
+function  App()  {
+  
+  return (
+    <div className="container">
+    
+    < HeaderTeam/>
+    < Team />
+    
+    
+    
+    </div>
+  );
+}
+export default App;
